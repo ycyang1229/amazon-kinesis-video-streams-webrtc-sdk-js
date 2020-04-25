c334b4400e73c4e2454e47e4a82bf4a9e1375e21
Mitchell Loeppky <mrl@amazon.com>
2020-04-24 18:15:34 -0700

    Example script that could be used for load testing

--- examples/viewer.js
+++ examples/viewer.js
@@ -1,29 +1,34 @@
-/**
- * This file demonstrates the process of starting WebRTC streaming using a KVS Signaling Channel.
- */
-const viewer = {};
+const REGION = 'us-west-2';
+const CREDENTIALS = {
+    accessKeyId: '',
+    secretAccessKey: ''
+}
+const CP_ENDPOINT = 'https://kinesisvideo.us-west-2.amazonaws.com';
+const CHANNEL_NAME = 'test-channel';
+const CLIENT_ID = 'test-client';
 
-async function startViewer(localView, remoteView, formValues, onStatsReport, onRemoteDataMessage) {
-    viewer.localView = localView;
-    viewer.remoteView = remoteView;
+const RUN_DELAY_MS = 1000;
+setInterval(RUN_DELAY_MS, () => {
+    run().then()
+});
 
-    // Create KVS client
-    const kinesisVideoClient = new AWS.KinesisVideo({
-        region: formValues.region,
-        accessKeyId: formValues.accessKeyId,
-        secretAccessKey: formValues.secretAccessKey,
-        sessionToken: formValues.sessionToken,
-        endpoint: formValues.endpoint,
-    });
+// Create KVS client
+const kinesisVideoClient = new AWS.KinesisVideo({
+    region: REGION,
+    accessKeyId: CREDENTIALS.accessKeyId,
+    secretAccessKey: CREDENTIALS.secretAccessKey,
+    endpoint: CP_ENDPOINT,
+});
+
+async function run() {
 
     // Get signaling channel ARN
     const describeSignalingChannelResponse = await kinesisVideoClient
         .describeSignalingChannel({
-            ChannelName: formValues.channelName,
+            ChannelName: CHANNEL_NAME, // Maybe use a non-constant here? You can also create the stream here by calling createSignalingChannel
         })
         .promise();
     const channelARN = describeSignalingChannelResponse.ChannelInfo.ChannelARN;
-    console.log('[VIEWER] Channel ARN: ', channelARN);
 
     // Get signaling channel endpoints
     const getSignalingChannelEndpointResponse = await kinesisVideoClient
@@ -39,13 +44,11 @@ async function startViewer(localView, remoteView, formValues, onStatsReport, onR
         endpoints[endpoint.Protocol] = endpoint.ResourceEndpoint;
         return endpoints;
     }, {});
-    console.log('[VIEWER] Endpoints: ', endpointsByProtocol);
 
     const kinesisVideoSignalingChannelsClient = new AWS.KinesisVideoSignalingChannels({
-        region: formValues.region,
-        accessKeyId: formValues.accessKeyId,
-        secretAccessKey: formValues.secretAccessKey,
-        sessionToken: formValues.sessionToken,
+        region: REGION,
+        accessKeyId: CREDENTIALS.accessKeyId,
+        secretAccessKey: CREDENTIALS.secretAccessKey,
         endpoint: endpointsByProtocol.HTTPS,
     });
 
@@ -56,9 +59,7 @@ async function startViewer(localView, remoteView, formValues, onStatsReport, onR
         })
         .promise();
     const iceServers = [];
-    if (!formValues.natTraversalDisabled && !formValues.forceTURN) {
-        iceServers.push({ urls: `stun:stun.kinesisvideo.${formValues.region}.amazonaws.com:443` });
-    }
+    iceServers.push({ urls: `stun:stun.kinesisvideo.${REGION}.amazonaws.com:443` });
     if (!formValues.natTraversalDisabled) {
         getIceServerConfigResponse.IceServerList.forEach(iceServer =>
             iceServers.push({
@@ -68,173 +69,47 @@ async function startViewer(localView, remoteView, formValues, onStatsReport, onR
             }),
         );
     }
-    console.log('[VIEWER] ICE servers: ', iceServers);
 
     // Create Signaling Client
-    viewer.signalingClient = new KVSWebRTC.SignalingClient({
+    const signalingClient = new KVSWebRTC.SignalingClient({
         channelARN,
         channelEndpoint: endpointsByProtocol.WSS,
-        clientId: formValues.clientId,
+        clientId: CLIENT_ID,
         role: KVSWebRTC.Role.VIEWER,
-        region: formValues.region,
-        credentials: {
-            accessKeyId: formValues.accessKeyId,
-            secretAccessKey: formValues.secretAccessKey,
-            sessionToken: formValues.sessionToken,
-        },
+        region: REGION,
+        credentials: CREDENTIALS,
     });
 
-    const resolution = formValues.widescreen ? { width: { ideal: 1280 }, height: { ideal: 720 } } : { width: { ideal: 640 }, height: { ideal: 480 } };
-    const constraints = {
-        video: formValues.sendVideo ? resolution : false,
-        audio: formValues.sendAudio,
-    };
-    const configuration = {
-        iceServers,
-        iceTransportPolicy: formValues.forceTURN ? 'relay' : 'all',
-    };
-    viewer.peerConnection = new RTCPeerConnection(configuration);
-    if (formValues.openDataChannel) {
-        viewer.dataChannel = viewer.peerConnection.createDataChannel('kvsDataChannel');
-        viewer.peerConnection.ondatachannel = event => {
-            event.channel.onmessage = onRemoteDataMessage;
-        };
-    }
-
-    // Poll for connection stats
-    viewer.peerConnectionStatsInterval = setInterval(() => viewer.peerConnection.getStats().then(onStatsReport), 1000);
-
-    viewer.signalingClient.on('open', async () => {
+    signalingClient.on('open', async () => {
         console.log('[VIEWER] Connected to signaling service');
 
-        // Get a stream from the webcam, add it to the peer connection, and display it in the local view
-        try {
-            viewer.localStream = await navigator.mediaDevices.getUserMedia(constraints);
-            viewer.localStream.getTracks().forEach(track => viewer.peerConnection.addTrack(track, viewer.localStream));
-            localView.srcObject = viewer.localStream;
-        } catch (e) {
-            console.error('[VIEWER] Could not find webcam');
-            return;
-        }
-
-        // Create an SDP offer to send to the master
-        console.log('[VIEWER] Creating SDP offer');
-        await viewer.peerConnection.setLocalDescription(
-            await viewer.peerConnection.createOffer({
-                offerToReceiveAudio: true,
-                offerToReceiveVideo: true,
-            }),
-        );
-
-        // When trickle ICE is enabled, send the offer now and then send ICE candidates as they are generated. Otherwise wait on the ICE candidates.
-        if (formValues.useTrickleICE) {
-            console.log('[VIEWER] Sending SDP offer');
-            viewer.signalingClient.sendSdpOffer(viewer.peerConnection.localDescription);
-        }
-        console.log('[VIEWER] Generating ICE candidates');
+        const offer = ''; // Create mock offer
+        signalingClient.sendSdpOffer(offer);
+        
+        const iceCandidates = []; // Create mock ice candidates
+        iceCandidates.forEach((candidate) => {
+            signalingClient.sendIceCandidate(candidate);
+        });
     });
 
-    viewer.signalingClient.on('sdpAnswer', async answer => {
-        // Add the SDP answer to the peer connection
+    signalingClient.on('sdpAnswer', async answer => {
         console.log('[VIEWER] Received SDP answer');
-        await viewer.peerConnection.setRemoteDescription(answer);
     });
 
-    viewer.signalingClient.on('iceCandidate', candidate => {
-        // Add the ICE candidate received from the MASTER to the peer connection
+    signalingClient.on('iceCandidate', candidate => {
         console.log('[VIEWER] Received ICE candidate');
-        viewer.peerConnection.addIceCandidate(candidate);
     });
 
-    viewer.signalingClient.on('close', () => {
+    signalingClient.on('close', () => {
         console.log('[VIEWER] Disconnected from signaling channel');
     });
 
-    viewer.signalingClient.on('error', error => {
+    signalingClient.on('error', error => {
         console.error('[VIEWER] Signaling client error: ', error);
     });
-
-    // Send any ICE candidates to the other peer
-    viewer.peerConnection.addEventListener('icecandidate', ({ candidate }) => {
-        if (candidate) {
-            console.log('[VIEWER] Generated ICE candidate');
-
-            // When trickle ICE is enabled, send the ICE candidates as they are generated.
-            if (formValues.useTrickleICE) {
-                console.log('[VIEWER] Sending ICE candidate');
-                viewer.signalingClient.sendIceCandidate(candidate);
-            }
-        } else {
-            console.log('[VIEWER] All ICE candidates have been generated');
-
-            // When trickle ICE is disabled, send the offer now that all the ICE candidates have ben generated.
-            if (!formValues.useTrickleICE) {
-                console.log('[VIEWER] Sending SDP offer');
-                viewer.signalingClient.sendSdpOffer(viewer.peerConnection.localDescription);
-            }
-        }
-    });
-
-    // As remote tracks are received, add them to the remote view
-    viewer.peerConnection.addEventListener('track', event => {
-        console.log('[VIEWER] Received remote track');
-        if (remoteView.srcObject) {
-            return;
-        }
-        viewer.remoteStream = event.streams[0];
-        remoteView.srcObject = viewer.remoteStream;
-    });
-
+    
     console.log('[VIEWER] Starting viewer connection');
-    viewer.signalingClient.open();
-}
-
-function stopViewer() {
-    console.log('[VIEWER] Stopping viewer connection');
-    if (viewer.signalingClient) {
-        viewer.signalingClient.close();
-        viewer.signalingClient = null;
-    }
-
-    if (viewer.peerConnection) {
-        viewer.peerConnection.close();
-        viewer.peerConnection = null;
-    }
-
-    if (viewer.localStream) {
-        viewer.localStream.getTracks().forEach(track => track.stop());
-        viewer.localStream = null;
-    }
-
-    if (viewer.remoteStream) {
-        viewer.remoteStream.getTracks().forEach(track => track.stop());
-        viewer.remoteStream = null;
-    }
-
-    if (viewer.peerConnectionStatsInterval) {
-        clearInterval(viewer.peerConnectionStatsInterval);
-        viewer.peerConnectionStatsInterval = null;
-    }
-
-    if (viewer.localView) {
-        viewer.localView.srcObject = null;
-    }
-
-    if (viewer.remoteView) {
-        viewer.remoteView.srcObject = null;
-    }
-
-    if (viewer.dataChannel) {
-        viewer.dataChannel = null;
-    }
-}
-
-function sendViewerMessage(message) {
-    if (viewer.dataChannel) {
-        try {
-            viewer.dataChannel.send(message);
-        } catch (e) {
-            console.error('[VIEWER] Send DataChannel: ', e.toString());
-        }
-    }
+    signalingClient.open();
+    
+    // At some point `signalingClient.close()` should be called
 }
